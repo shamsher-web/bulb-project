0a83185713e858fd9afd1891d262267b364169b6
shamsher-web <77044946+shamsher-web@users.noreply.github.com>
2022-10-15 10:43:28 +0530

    adder html and js file for the bulb

---
+++ index.js
@@ -0,0 +1,18 @@
+function myFun1() {
+    
+    imgId.src="images/bulbon.gif";
+
+    }
+    
+
+ function myFun2(){
+    
+    imgId.src="images/bulb_off.gif";
+ } 
+
+myFun1();
+myFun2();
+
+ 
+
+
